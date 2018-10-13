5b002ca6ebd7c04bb20740ea0ba352d48468e9be
Jan-Philipp <jpm3492@gmail.com>
2018-10-13 16:39:30 +0200

    done

--- index.js
+++ index.js
@@ -23,8 +23,6 @@ if (process.env.NODE_ENV === 'production') {
   app.get('*', (req, res) => { res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')) })
 }
 
-require('./websockets')
-
 const port = process.env.PORT || 4000
 const server = http.createServer(app)
 
